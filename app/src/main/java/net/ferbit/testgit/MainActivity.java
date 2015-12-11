19782dc58fa3c3fe9a9019f8d223f7b917d75fb8
luckymate <matej@rusevci.si>
2015-12-11 07:28:20 +0100

    matej

--- app/src/main/java/net/ferbit/testgit/MainActivity.java
+++ app/src/main/java/net/ferbit/testgit/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "matej";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Log.d(TAG, "onOptionsItemSelected: kaj");
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
